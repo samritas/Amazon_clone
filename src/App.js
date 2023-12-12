64da2564f5c921201a095a699d5bdf7a92abb465
samritas <145132973+samritas@users.noreply.github.com>
2023-12-13 02:57:00 +0300

    the product page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import AppIndex from "./components";
+import Checkout from "./components/Checkout";
 import Header from "./components/Header";
 import Home from "./components/Home";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-  <AppIndex/>)
+    <Router>
+        <>
+        <Routes>
+            <Route path="/" element={<><Header/><Home/></>}/>
+            <Route path="/checkout" element={<><Header/><Checkout/></>}/>
+        </Routes>
+        </>
+    </Router>
+    )
 }
 
 export default App;
---
+++ src/components/Checkout.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './chekout.css'
+function Checkout() {
+  return (
+    <div classname='checkout' style={{display:'flex',height: 'max-content',padding: '20px'}}>
+      <div className='checkout_left'>
+        <img
+         className="checkout__ad"
+         src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+         alt=""
+        />
+        <h3>Hello</h3>
+        <h2 className="checkout__title">Your shopping Basket</h2>
+
+      </div>
+      <div className='checkout_right' style={{width:'100%'}}>
+        <h2>subtotal</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Checkout
